1400f3b7f147f35c1b82b1c53310e75f5c7288f6
Tú Anh lê <letuanh2403@gmail.com>
2020-08-07 17:37:27 +0700

    day1

---
+++ Core.Data/EF/CoreDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Core.Data.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Data.EF
+{
+    public class CoreDBContext: DbContext
+    {
+        public CoreDBContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
